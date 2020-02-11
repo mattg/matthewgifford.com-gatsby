bc988c587766d63a92a6215aef987cfae7845ac2
Matthew Gifford <mgifford@goldstar.com>
2020-02-10 23:05:56 -0800

    Get started

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Matthew Gifford</h1>
+    <p>Dad to <a href="http://www.augustgifford.com/">August</a> and <a href="http://www.hazelgifford.com/">Hazel</a>. Husband to <a href="http://www.thegiffordranch.com/">Angela</a>. Developer at <a href="https://www.goldstar.com/">Goldstar</a>. DM for Dragon Hunters Intercontinental. Maker of old fashioneds.</p>
+    <h2>Links</h2>
+    <ul>
+      <li><Link to="/newsletter/">Giff’s Miscellanea</Link> (my email newsletter)</li>
+      <li><Link to="/dnd-resources/">D&amp;D resources</Link></li>
+      <li><a href="https://github.com/mattg">GitHub</a></li>
+      <li><a href="https://keybase.io/giff">Keybase</a></li>
+      <li><a href="mailto:mgifford@gmail.com">email</a></li>
+      <li><a href="http://www.amazon.com/gp/registry/wishlist/O31YDA173GFW/ref=cm_wl_rlist_go_o?sort=priority&amp;itemPerPage=25">wish list</a></li>
+      <li><a href="https://cash.me/$matthewgifford">Square Cash</a></li>
+    </ul>
   </Layout>
 )
 
